c50908d32afbe2601470668d1f9f345be20acf19
paminus-kingori <pkmurungi.pm@gmail.com>
2020-04-16 21:50:13 +0300

    Edited the profile page

--- devcodes/settings.py
+++ devcodes/settings.py
@@ -78,12 +78,9 @@ WSGI_APPLICATION = 'devcodes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': "devcode",
-        "HOST":"127.0.0.1",
-        "PORT":3306,
-        "USER":"root",
-        "PASSWORD":'',
+        
     }
 }
 
