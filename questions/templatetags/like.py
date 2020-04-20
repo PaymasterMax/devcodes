f1d6e5a347d53c391aa3cf0ba70fdec9bb34d066
PaymasterMax <duncansantiago18@gmail.com>
2020-04-20 13:34:41 -0700

    Bug fix

--- questions/templatetags/like.py
+++ questions/templatetags/like.py
@@ -6,10 +6,13 @@ register = template.Library()
 
 @register.filter("likey")
 def liked(curuid , qidobj):
+    controller = False
+
+    # look for the match
     for x in qidobj.question_liked.all():
         # Test if the current user has liked this Queation
         if curuid == x.luid_id:
-            return True
-
-        else:
-            return False
+            controller = True
+            break
+            
+    return controller
