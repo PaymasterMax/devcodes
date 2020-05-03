99be0241938917985f2f2dfbb3f34d1c96e84f39
PaymasterMax <duncansantiago18@gmail.com>
2020-05-03 14:14:34 -0700

    updates

--- devcodes/settings.py
+++ devcodes/settings.py
@@ -1,5 +1,6 @@
 import os
 import django_heroku , dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,8 +41,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,6 +83,10 @@ TEMPLATES = [
 ]
 
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 AUTHENTICATION_BACKENDS = (
     # 'social_core.backends.github.GithubOAuth2',
     # 'social_core.backends.twitter.TwitterOAuth',
@@ -97,19 +102,18 @@ WSGI_APPLICATION = 'devcodes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': "devcodes.sqlite3",
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': "devcodes",
-        # "PORT":3306,
-        # "HOST":"127.0.0.1",
-        # "USER":"root",
-        # "PASSWORD":"",
 
-    }
-}
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': "devcodes.sqlite3",
+#     }
+# }
 
 
 # Password validation
@@ -159,6 +163,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR , "peer/static"),
     os.path.join(BASE_DIR , "chatroom/static"),
 ]
+django_heroku.settings(locals())
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , "media_root")
@@ -174,7 +181,4 @@ LOGIN_REDIRECT_URL = 'http://localhost:8000/'
 
 # SOCIAL_AUTH_GITHUB_KEY = '400c9120d85c04fe1d27'
 # SOCIAL_AUTH_GITHUB_SECRET = 'b5062525aedc28521c3df0f1c878e61ac4ecb52d'
-
-django_heroku.settings(locals())
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+del DATABASES['default']['OPTIONS']['sslmode']
