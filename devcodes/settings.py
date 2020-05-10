764e2b8370700bdaa07766ad300413988b9cdedc
PaymasterMax <duncansantiago18@gmail.com>
2020-05-10 03:46:34 -0700

    updates

--- devcodes/settings.py
+++ devcodes/settings.py
@@ -2,6 +2,8 @@ import os
 import django_heroku , dj_database_url
 import dotenv
 import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
