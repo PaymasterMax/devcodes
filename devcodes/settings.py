92324560abc8992c9ea70470ce0bad8397720751
PaymasterMax <duncansantiago18@gmail.com>
2020-05-10 03:57:43 -0700

    updates

--- devcodes/settings.py
+++ devcodes/settings.py
@@ -106,6 +106,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'uprofile.context_processors.consts',
 
                 # 'social_django.context_processors.backends',
                 # 'social_django.context_processors.login_redirect',
