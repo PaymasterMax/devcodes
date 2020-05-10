2fa36ff8ae06297dbe0df6d5aea5d0a9fb803bf7
PaymasterMax <duncansantiago18@gmail.com>
2020-05-09 17:16:43 -0700

    Bug fix

--- devcodes/settings.py
+++ devcodes/settings.py
@@ -15,7 +15,7 @@ prod_db  =  dj_database_url.config(conn_max_age=500)
 SECRET_KEY = 'ghr%=q!3afx83t+1dojl%j15mta%6!v@bpx2l5p+ki2i!)zqs3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
- DEBUG = True
+DEBUG = True
 #DEBUG = False
 
 SESSION_COOKIE_SECURE = True
