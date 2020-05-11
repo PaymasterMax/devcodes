8204d8e87e02a712a605e283f8bc15fc7628af40
PaymasterMax <duncansantiago18@gmail.com>
2020-05-11 01:59:59 -0700

    bug fix

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ import cloudinary.api
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# DATABASES['default'].update(prod_db)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
