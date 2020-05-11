9f141042c59ffd54b1d6ca8098a84ee4cdfaaea8
PaymasterMax <duncansantiago18@gmail.com>
2020-05-11 15:02:09 -0700

    bug fix

--- chatroom/models.py
+++ chatroom/models.py
@@ -16,7 +16,7 @@ class ChatModel(models.Model):
 # feedback
 class FeedBack(models.Model):
     fid = models.AutoField(primary_key = True ,  serialize = True)
-    feedback_sender = models.EmailField(max_length = 40)
+    feedback_sender = models.EmailField(max_length = 40 , default = "Anornymous@gmail.com")
     feedback = models.TextField(max_length = 400 , default = "")
     post_time = models.DateTimeField(auto_now_add = True)
 
