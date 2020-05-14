006405195b20d50a35829e57762246cc1f4ce67d
PaymasterMax <duncansantiago18@gmail.com>
2020-05-14 13:43:59 -0700

    migarted bug

--- login/models.py
+++ login/models.py
@@ -3,5 +3,5 @@ from signup.models import Signup as sv
 # Create your models here.
 class recoverdata(models.Model):
     id = models.AutoField(primary_key = True , serialize = True)
-    uid = models.ForeignKey(sv , related_name = "recnewdb" , on_delete = models.CASCADE)
-    secret_code = models.CharField(max_length = 10)
+    uid = models.ForeignKey(sv , related_name = "recnewdb" , on_delete = models.CASCADE , default = 1)
+    secret_code = models.CharField(max_length = 10 , default = [*range(10)]])
