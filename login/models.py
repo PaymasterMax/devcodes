6a81ff9305af9cd24bf06fbfbbab2edf309fa4e4
PaymasterMax <duncansantiago18@gmail.com>
2020-05-14 13:39:43 -0700

    bug fix

--- login/models.py
+++ login/models.py
@@ -2,12 +2,6 @@ from django.db import models
 from signup.models import Signup as sv
 # Create your models here.
 class recoverdata(models.Model):
-    """docstring for recoverdata."""
-
-    def __init__(self, arg):
-        super(recoverdata, self).__init__()
-        self.arg = arg
-
     id = models.AutoField(primary_key = True , serialize = True)
     uid = models.ForeignKey(sv , related_name = "recnewdb" , on_delete = models.CASCADE)
     secret_code = models.CharField(max_length = 10)
