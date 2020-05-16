9ed700dc862350b347fd129a84118afa70e5ea42
PaymasterMax <duncansantiago18@gmail.com>
2020-05-16 10:11:00 -0700

    bug fix

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
 app_name = "home"
+
 urlpatterns = [
-    url("^$" , views.homeview , name = "home")]
+    url("^$" , views.homeview , name = "home")
+    ]
