a2077e39d115250ffe94606e2a28f21c21375ac7
PaymasterMax <duncansantiago18@gmail.com>
2020-05-20 14:22:05 -0700

    patch

--- signup/static/signup/devcodes.js
+++ signup/static/signup/devcodes.js
@@ -97,12 +97,14 @@ else if(document.getElementById("hobby").value == "")
 					{
 						document.getElementById("pass1").innerHTML = "";
 						document.getElementById("pass2").innerHTML = "";
+						emp.setAttribute("style" , "display:block")
 						emp.innerHTML = "password cannot be empty";
 					}
 				else if(pass1 != pass2)
 					{
 						document.getElementById("pass1").innerHTML = "";
 						document.getElementById("pass2").innerHTML = "";
+						mis.setAttribute("style" , "display:block")
 						mis.innerHTML = "Password mismatch";
 					}
 
@@ -111,6 +113,7 @@ else if(document.getElementById("hobby").value == "")
 						{
 							document.getElementById("pass2").innerHTML = "";
 							document.getElementById("pass1").innerHTML = "";
+							emp.setAttribute("style" , "display:block")
 							emp.innerHTML = "Short password";
 						}
 					}
