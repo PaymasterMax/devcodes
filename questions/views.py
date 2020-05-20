afdba94bc19c507a740009a2c3f3bfec302ed4ee
PaymasterMax <duncansantiago18@gmail.com>
2020-05-20 09:49:28 -0700

    bug fix

--- questions/views.py
+++ questions/views.py
@@ -9,9 +9,15 @@ from django.http import JsonResponse , HttpResponse
 
 def questionsview(request):
     all_questions = Questions.objects.all().annotate(no_of_answers = Count("question_to_answer")).order_by("-time_posted")
-
-    return render(request , "questions/questions.html" , context = {"Questions":all_questions , "userlog":userlog})
-
+    try:
+        userdetails = Signup.objects.get(username = request.session["username"])
+        newmessage = chtb.objects.filter(r2uid_id =  userdetails.uid, bell_seen = False).count()
+        userlog = True
+    except Exception as e:
+        userlog = False
+        request.session["redirect"] = "/questions/"
+        return redirect("/login/")
+        return render(request , "questions/questions.html" , context = {"Questions":all_questions , "userlog":userlog})
     else:
         return render(request , "questions/questions.html" , context = {"Questions":all_questions , "mydetails":userdetails , "newmessage":newmessage , "userlog":userlog})
 
