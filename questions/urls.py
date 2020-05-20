444a400f99fa96ec90014185701c4e1cb9ac26d4
PaymasterMax <duncansantiago18@gmail.com>
2020-05-20 09:20:04 -0700

    patch

--- questions/urls.py
+++ questions/urls.py
@@ -4,7 +4,6 @@ app_name = "questions"
 
 urlpatterns = [
     url("^$" , views.questionsview , name = "questions"),
-    url("^qupdater/" , views.qupdater , name = "qupdater"),
     url("^feedback/$" , views.feed , name = "feedback"),
     url("^updatelikes/$" , views.updatelikes , name = "updatelikes"),
     url("^myquestions/$" , views.myquestions , name = "myquestions"),
