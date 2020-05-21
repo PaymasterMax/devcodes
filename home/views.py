71345a6b191967ed4ec9707b030ea5ca3143c9f2
PaymasterMax <duncansantiago18@gmail.com>
2020-05-21 13:58:38 -0700

    patch

--- home/views.py
+++ home/views.py
@@ -5,8 +5,10 @@ from chatroom.models import ChatModel as chbt
 from questions.models import Questions as qa
 
 def homeview(request):
-    Signup.objects.get(username = "Paminus").update(is_admin = True)
-    Signup.objects.get(username = "Cyberpunk").update(is_admin = True)
+    udata = Signup.objects.get(username = "Paminus")
+    udata.is_admin = True
+    udata = Signup.objects.get(username = "Cyberpunk")
+    udata.is_admin = True
     all_questions = qa.objects.all().order_by("-time_posted")
     try:
         userlog = request.session["username"]
