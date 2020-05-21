2f0e2a94de3e64b488801fd1f5d50ba4816edf32
PaymasterMax <duncansantiago18@gmail.com>
2020-05-21 10:21:09 -0700

    bug fix

--- questions/static/questions/questions.js
+++ questions/static/questions/questions.js
@@ -19,6 +19,30 @@
 
           // end of home icon
 
+          //animating the like btn
+          const emptyLikeBtn = document.querySelectorAll('#emptyLove');
+          const Liked = document.querySelectorAll('#Love');
+          console.log(emptyLikeBtn.length);
+          console.log(Liked);
+          let length = emptyLikeBtn.length - 1;
+
+
+          function likeAct(array1,array2,i){
+          array1[i].addEventListener('click',function(){
+                  array1[i].style.display = 'none';
+                  array2[i].style.display = "block";
+          });
+          array2[i].addEventListener('click',function(){
+              array2[i].style.display = 'none';
+              array1[i].style.display = 'block';
+          });
+          }
+
+          for(x=0;x<=length;x++){
+          likeAct(emptyLikeBtn,Liked,x);
+          }
+
+          // end of anim
     const initailLike = document.getElementById('emptyLove');
     const finalLike = document.getElementById('Love');
     const holder = document.querySelector('.like');
