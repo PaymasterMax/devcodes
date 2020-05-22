05dfd66dba9ec6a03efb170ab0b30bb4f768aa58
PaymasterMax <duncansantiago18@gmail.com>
2020-05-22 05:36:35 -0700

    bug fix

--- chatroom/templatetags/timemod.py
+++ chatroom/templatetags/timemod.py
@@ -54,6 +54,6 @@ def msagecount(msguid , msguid2):
 
 @register.filter("username_modifier")
 def usermodifier(username):
-    username = username.split(" ")[0]
+    # username = username.split(" ")[0]
     print("\n\n\n\n\n\n{}".format(username))
     return username
