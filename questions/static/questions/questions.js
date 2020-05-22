5479b23cce9a2223a394a22f0492961c2ff0b87c
PaymasterMax <duncansantiago18@gmail.com>
2020-05-22 02:37:12 -0700

    patch

--- questions/static/questions/questions.js
+++
@@ -1,9 +0,0 @@
-  function checkmesages() {
-    try {
-      var xmlobj = new XMLHttpRequest();
-    } catch (e) {
-      var xmlobj = new ActiveXObject();
-    } finally {
-      var msgcount = 0;
-    }
-  }
