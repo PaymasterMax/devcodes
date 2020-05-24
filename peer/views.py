fbac076961298c1a30d7a6fec837955443ceac2d
PaymasterMax <duncansantiago18@gmail.com>
2020-05-24 11:27:20 -0700

    patch

--- peer/views.py
+++ peer/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 def peers_suggest_per_lang(userinfo):
     try:
-        users = sp.objects.exclude(username = userinfo.username)
+        users = sp.objects.exclude(username = userinfo.username).filter(hobby != userinfo.hobby)
         rest_of_users = sp.objects.exclude(username = userinfo.username).filter(hobby__ne = userinfo.hobby)
         users = users | rest_of_users
     except Exception as e:
