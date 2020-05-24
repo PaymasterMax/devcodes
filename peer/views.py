1af16be0996f91776c96fa29e4300e8112246693
PaymasterMax <duncansantiago18@gmail.com>
2020-05-24 11:39:03 -0700

    bug fix

--- peer/views.py
+++ peer/views.py
@@ -6,8 +6,8 @@ from django.http import HttpResponse
 def peers_suggest_per_lang(userinfo):
     try:
         print(userinfo.hobby)
-        users = sp.objects.exclude(username = userinfo.username).filter(hobby__ne= userinfo.hobby)
-        rest_of_users = sp.objects.exclude(username = userinfo.username).filter(hobby__ne = userinfo.hobby)
+        users = sp.objects.exclude(username = userinfo.username).filter(hobby= userinfo.hobby)
+        rest_of_users = sp.objects.exclude(username = userinfo.username).exclude(hobby = userinfo.hobby)
         users = users | rest_of_users
     except Exception as e:
         print("\n\n\n\n\n\n\n{}".format(e))
