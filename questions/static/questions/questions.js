b67e68a1e9df2511ba73bd4c1577eac9d15b351e
GUSERNAME <GEMAIL>
2020-07-26 01:23:20 -0700

    patch auto-gen by {/usr/local/sbin/gm:-2} under account root

--- questions/static/questions/questions.js
+++ questions/static/questions/questions.js
@@ -122,3 +122,22 @@ deleteBtn.addEventListener('click',function(){
     overlay.classList.remove('active')
     }
   }
+
+  function deleteQuestion(target , id) {
+    alert("Hello world");
+    var target = document.querySelector("."+target);
+    try {
+      var xmlhttp = new XMLHttpRequest();
+    } catch (e) {
+      var xmlhttp = new ActiveXObject();
+    } finally {
+      xmlhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200) {
+          target.remove();
+        }
+      }
+      xmlhttp.setRequestHeader("X-CSRFToken" , "{{csrf_token}}");
+      xmlhttp.open("POST"  , "{% url 'questions:deleteQuestion' %}", true );
+      xmlhttp.send("Qid="id);
+    }
+  }
