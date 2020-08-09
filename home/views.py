3d0b9c2b83a6e8bf20e01989842f3e527e498b3d
GUSERNAME <GEMAIL>
2020-08-09 03:05:55 -0700

    patch auto-gen by /usr/local/sbin/gm under account root

--- home/views.py
+++ home/views.py
@@ -5,20 +5,13 @@ from chatroom.models import ChatModel as chbt
 from questions.models import Questions as qa
 
 def homeview(request):
-    # try:
-    #     print("\n\n\n\n\n".format(request.user))
-    #     request.session["username"] = request.user
-    #     request.session["loginstatus"] = True
-    #     request.session.set_expiry(0)
-    # except Exception as e:
-    #     pass
     all_questions = qa.objects.all().order_by("-time_posted")
     try:
         userlog = request.session["username"]
         userinfo = Signup.objects.get(username = userlog)
         notifs = chbt.objects.filter(r2uid_id = userinfo.uid , bell_seen = False).count()
     except Exception as e:
-        # userlog = False
+        userlog = False
         return render(request , "home/home.html" , context = {"allq": all_questions , "userlog":userlog})
 
     else:
