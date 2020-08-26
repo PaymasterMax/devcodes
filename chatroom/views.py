492db541318173495fe4e8803666970ce5977969
GUSERNAME <GEMAIL>
2020-08-25 19:52:49 -0700

    patch auto-gen by /usr/local/sbin/gm under account root

--- chatroom/views.py
+++ chatroom/views.py
@@ -34,7 +34,7 @@ def inbox(request):
 
         else:
             all_messages = chatmod.objects.filter(Q(r2uid_id = userdetails.uid) | Q(r1uid_id = userdetails.uid)).order_by("-text_time")
-
+            print("\n\n\nfirst{}".format(e))
             all_messages = db_unique(all_messages , userdetails.uid)
             return render(request , "chatroom/inbox.html/" , context = {"all_messages":all_messages , "userdetails":userdetails , "newmessage":newmessage , "userlog":userlog})
     except Exception as e:
